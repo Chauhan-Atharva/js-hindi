e2f9848aa96781cf91241e9ff85f346890425ed1
Chauhan-Atharva <atharva.st.05@gmail.com>
2025-08-30 17:37:31 +0000

    oops  2

--- 10_classes_and_oop/oops.js
+++ 10_classes_and_oop/oops.js
@@ -17,7 +17,34 @@ console.log(user["singnedIn"]);
 user.getUserDetails();
 
 //constructor function 
-co
+// const promise1 = new Promise();//new is constructor func 
+const date = new Date();//creates instances/copy of object literal
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+    this.greetings = function (){
+        console.log(`Welcome ${this.username}`);
+    }
+    // return (this); - even if you don't write this 
+    //this will always be returned - it is implicitly defined
+}
+const user1 = new User("hitesh", 23, true);
+const user2=  new User("chai aur code", 45, false);//overrides the value given by user1
+console.log(user1);
+console.log(user2);
+console.log(user1.constructor);//gives a reference to User function 
+console.log(user1 instanceof User);//true
+console.log(user1 instanceof Object);//true - all objects are from Object 
+
+
+
+//new keyword -> a new empty object is created 
+//a constructor func is called 
+//all the arguments are then injected 
+//you get the result
+
 
 
 
